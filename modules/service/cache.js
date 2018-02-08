883ff6fffbcbf9cf23fdee382080e3a9cf5aad85
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-08 08:48:13 -0500

    Implemented cache connection class

---
+++ modules/service/cache.js
@@ -0,0 +1,40 @@
+// kiwi/relva/modules/db.js - Created February 6th, 2018
+
+// NPM Modules
+const redis = require('redis');
+
+// Custom Modules
+const log = require('../util/log.js');
+
+class RedisConnection {
+    /**
+     * options:
+     *  - host (string: redis host address)
+     *  - port (int: redis host listen port)
+     *  - password (string: redis auth password)
+     *  - db (string: database to use within redis)
+     */
+    constructor(options) {
+        this.connection = redis.createClient(options);
+
+        this.connection.on("error", function(err) {
+            log(`[cache] Error: ${err}`);
+            process.exit(1);
+        });
+
+        this.connection.on("connect", function() {
+            log(`[cache] KIWI-Cache connection established`);
+        });
+
+        this.connection.on("ready", function() {
+            //this.connection.auth(options.password);
+            log(`[cache] KIWI-Cache ready`);
+        });
+    }
+
+    conn() {
+        return this.connection;
+    }
+}
+
+module.exports = RedisConnection;
