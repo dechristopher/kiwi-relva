cf55f61c1669c62ab2b61346be44a1ba7063249a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-08 08:53:19 -0500

    Reference CHANGELOG v1.3.0 for changes (user.js)

--- modules/user.js
+++ modules/user.js
@@ -1,28 +1,100 @@
 // kiwi/relva/modules/user.js - Created February 6th, 2018
 
-class User {
-    constructor(bot, dbOptions) {
-        this.bot = bot;
-        //this.dbc = new(require('./service/dbc.js'))(dbOptions);
-    }
+// Custom Modules
+const log = require('./util/log.js');
 
-    isLinked(userID) {
-        return this.checkLinked(userID);
+class User {
+    constructor(bot, dbOptions, cacheOptions, debug = false) {
+        this.bot = bot;
+        this.dbc = new(require('./service/dbc.js'))(dbOptions);
+        this.cache = new(require('./service/cache.js'))(cacheOptions);
+        this.debug = debug;
     }
 
     checkLinked(userID) {
-        // hit db
-        return true;
+        let conn = this.dbc.conn();
+        return new Promise(function(resolve, reject) {
+            conn.query('SELECT steam_id FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+                if (error) {
+                    reject(error);
+                }
+                //console.log(`${userID} ${results} ${results[0].steam_id !== undefined}`);
+                if (results.length === 1) {
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            });
+        });
     }
 
     linkAccount(userID, steamID) {
-        console.log('hit linkAccount');
-        return `Linked SteamID: ${steamID} to user ID: ${userID}`;
+        return `[FAKE] Linked SteamID: ${steamID} to user ID: ${userID}`;
     }
 
+    // Non-promisified, happens regardles of command running chain
     avatarAssoc(userID, avatarURL) {
-        //this.dbc.conn().query("TODO");
+        let dbconn = this.dbc.conn();
+        let cconn = this.cache.conn();
+        let debug = this.debug;
+        cconn.hexists('avatars', userID, function(err, reply) {
+            if (debug) { log(`Avatar Exists (${userID}): ${reply}`); }
+            if (reply === 1) {
+                cconn.hget('avatars', userID, function(err, reply) {
+                    if (reply === avatarURL) {
+                        // Do nothing :D
+                        if (debug) { log(`Avatar for ${userID} already set properly. Not hitting db.`); }
+                        return;
+                    } else {
+                        dbconn.query('UPDATE `users` SET discord_avatar_url = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
+                            if (debug) { log(`Set ${userID} avatar to ${avatarURL} in db`); }
+                        });
+                        cconn.hset('avatars', userID, avatarURL, function(err, reply) {
+                            if (debug) { log(`Set ${userID} avatar to ${avatarURL} in cache`); }
+                        });
+                    }
+                });
+            } else {
+                dbconn.query('UPDATE `users` SET discord_avatar_url = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
+                    if (debug) { log(`Set ${userID} avatar to ${avatarURL} in db`); }
+                });
+                cconn.hset('avatars', userID, avatarURL, function(err, reply) {
+                    if (debug) { log(`Set ${userID} avatar to ${avatarURL} in cache`); }
+                });
+            }
+        });
     }
+
+    getUserSteamID(userID) {
+        this.dbc.conn().query('SELECT steam_id FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+            return results[0].steam_id;
+        });
+    }
+
+    getUserSteamID64(userID) {
+        this.dbc.conn().query('SELECT steam_id_64 FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+            return results[0].steam_id_64;
+        });
+    }
+
+    getUserSteamID3(userID) {
+        this.dbc.conn().query('SELECT steam_id_3 FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+            return results[0].steam_id_3;
+        });
+    }
+
+    getUserSteamProfile(userID) {
+        this.dbc.conn().query('SELECT steam_profile_url FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+            return results[0].steam_profile_url;
+        });
+    }
+
+    getUserDiscordAvatar(userID) {
+        this.dbc.conn().query('SELECT discord_avatar_url FROM `users` WHERE `discord_id` = ?', [userID], function(error, results, fields) {
+            return results[0].discord_avatar_url;
+        });
+    }
+
 }
 
 module.exports = User;
