480b3fd5100c25d01f0ed67fbfc750ce85202eac
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-10 17:15:39 -0500

    Modularized avatarAssoc function in modules/ops/avatarAssoc.js

---
+++ modules/ops/avatarAssoc.js
@@ -0,0 +1,48 @@
+// kiwi/relva/modules/ops/avatarAssoc.js - Created February 6th, 2018
+
+// Custom Modules
+const log = require('../util/log.js');
+
+/*
+    Avatar Association Algorithm: doc/avatar-association-algorithm.txt
+    - Non-promisified, happens regardles of command runner event loop
+    - userID (string): the user's Discord ID
+    - avatarURL (string): link to the user's Discord avatar
+    - dbconn (Object): reference to the database connection
+    - cconn (Object): reference to the cache connection
+    - debug (boolean): whether or not to show debug output
+*/
+module.exports = function(userID, avatarURL, dbconn, cconn, debug) {
+    // Check to see if the cache contains an entry for the user's avatar
+    cconn.hexists('avatars', userID, function(err, reply) {
+        if (debug) { log(`Avatar Exists (${userID}): ${reply}`); }
+        if (reply === 1) {
+            // Get the avatar URL from the cache to check if it's the same
+            cconn.hget('avatars', userID, function(err, reply) {
+                if (reply === avatarURL) {
+                    // Do nothing :D
+                    if (debug) { log(`Avatar for ${userID} already set properly. Not hitting db.`); }
+                    return;
+                } else {
+                    // Update the changed avatar in the database
+                    dbconn.query('UPDATE `users` SET discord_avatar_url = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
+                        if (debug) { log(`Set ${userID} avatar to ${avatarURL} in db`); }
+                    });
+                    // Update the changed avatar in the cache
+                    cconn.hset('avatars', userID, avatarURL, function(err, reply) {
+                        if (debug) { log(`Set ${userID} avatar to ${avatarURL} in cache`); }
+                    });
+                }
+            });
+        } else {
+            // Set the avatar in the database
+            dbconn.query('UPDATE `users` SET discord_avatar_url = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
+                if (debug) { log(`Set ${userID} avatar to ${avatarURL} in db`); }
+            });
+            // Set the avatar in the cache
+            cconn.hset('avatars', userID, avatarURL, function(err, reply) {
+                if (debug) { log(`Set ${userID} avatar to ${avatarURL} in cache`); }
+            });
+        }
+    });
+};
