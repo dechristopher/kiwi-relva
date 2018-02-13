3aea7b508cc9c292ca0d66c590bc0472264798ed
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-13 00:35:54 -0500

    Added op module to get a user's CS:GO hours (grabCSGOHours.js)

---
+++ modules/ops/grabCSGOHours.js
@@ -0,0 +1,28 @@
+// kiwi/relva/modules/ops/grabSteamID.js - Created February 6th, 2018
+
+// NPM Modules
+const got = require('got');
+
+// Custom Modules
+const log = require('../util/log.js');
+
+/*
+    CS:GO Hours Grabber
+    - steamID (string): the Steam ID of the user in question
+
+    Returns: (number): hours(rounded)
+    Returns: -1 if account doesn't own CS:GO
+*/
+module.exports = function(steamID) {
+    return new Promise(function(resolve, reject) {
+        // Snag some info from random API we found to get hours
+        got(`https://beta.decapi.me/steam/hours?id=${steamID}&appid=730`).then(resp => {
+            if (resp.body.split(' ').indexOf('game') > -1) {
+                resolve(-1);
+            }
+            let hours = parseInt(resp.body.split(' ')[0]);
+            log(hours);
+            resolve(hours);
+        });
+    });
+};
