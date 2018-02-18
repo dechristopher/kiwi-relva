f384296eb826ce782832e6fde3f60f6a1a5c8ec7
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-17 23:55:43 -0500

    Implemented ID generation module genID.js

---
+++ modules/util/genID.js
@@ -0,0 +1,17 @@
+// kiwi/relva/modules/util/genID.js - Created February 17th, 2018
+
+/**
+ * Returns a randomly generated string of length n beginning with `k`
+ * that will not contain any other `k`
+ * @param {number} n the length of the generated id
+ * @param {Object} params generation parameters
+ */
+module.exports = function(n, params = { prefix: true }) {
+    let id = params.prefix ? 'k' : '';
+    let length = params.prefix ? n : n + 1;
+    let dict = "abcdefghijlmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < (length - 1); i++) {
+        id += dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    return id;
+};
