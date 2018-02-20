fcc5375776049afa18d985f804780b202e611027
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-19 22:51:30 -0500

    Implemented debug logging module modules/util/dlog.js

---
+++ modules/util/dlog.js
@@ -0,0 +1,18 @@
+// kiwi/relva/modules/util/dlog.js - Created February 19th, 2018
+
+// NPM Modules
+const log = require('./log.js');
+
+/**
+ * Logs a debug message to STDOUT only if debugging
+ * @param {string} message a debug message to log
+ * @returns {Promise<string>} a promise containing the string that was logged
+ * @returns {Promise<error>} on log rejection
+ */
+module.exports = (message) => {
+    if (process.env.DEBUG) {
+        return new Promise(function(resolve, reject) {
+            log(message).then(() => resolve(message)).catch(error => reject(error));
+        });
+    }
+};
