27393b09e525fa9956210b3cb8d15f1e1e401477
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:38:12 -0500

    ESLint cleanup - checkLinked.js

--- modules/ops/checkLinked.js
+++ modules/ops/checkLinked.js
@@ -10,17 +10,17 @@ const log = require('../util/log.js');
  * @param {boolean} debug debug mode
  * @returns {Promise<boolean>} whether or not the user has a Steam account linked and username set
  */
-module.exports = function(userID, dbconn, debug) {
-    return new Promise(function(resolve, reject) {
-        // Ensure user has a profile linked and has set their username
-        dbconn.query("SELECT steam_id FROM `users` WHERE `discord_id` = ? AND `username` > ''", [userID], function(error, results, fields) {
-            //if (debug) { log(`${userID} ${results} ${results[0].steam_id !== undefined}`); }
-            if (error) {
-                log('checkLinked Error: ' + error);
-                resolve(`Sorry, something weird happened on our end. Contact \`<@119966322523242497>\` immediately and try again shortly. \`[CODE: K68]\``);
-                return;
-            }
-            resolve((results.length === 1) ? true : false);
-        });
-    });
+module.exports = function(userID, dbconn) {
+	return new Promise(function(resolve) {
+		// Ensure user has a profile linked and has set their username
+		dbconn.query('SELECT steam_id FROM `users` WHERE `discord_id` = ? AND `username` > \'\'', [userID], function(error, results) {
+			// if (debug) { log(`${userID} ${results} ${results[0].steam_id !== undefined}`); }
+			if (error) {
+				log('checkLinked Error: ' + error);
+				resolve('Sorry, something weird happened on our end. Contact `<@119966322523242497>` immediately and try again shortly. `[CODE: K68]`');
+				return;
+			}
+			resolve((results.length === 1) ? true : false);
+		});
+	});
 };
