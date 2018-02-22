3b5201b5d3e2d184a0c191570c8ed70936aea3d4
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:38:46 -0500

    ESLint cleanup - checkValidSteamProfileURL.js

--- modules/ops/checkValidSteamProfileURL.js
+++ modules/ops/checkValidSteamProfileURL.js
@@ -1,8 +1,5 @@
 // kiwi/relva/modules/ops/checkValidSteamProfileURL.js - Created February 14th, 2018
 
-// NPM Modules
-const regex = require('regex');
-
 // Custom Modules
 const dlog = require('../util/dlog.js');
 
@@ -12,13 +9,13 @@ const dlog = require('../util/dlog.js');
  * @returns {Promise<boolean>} whether or not the Steam profuile URL is valid
  */
 module.exports = function(steamURL) {
-    // Steam Profile URL Regex
-    let steamURLReg = new RegExp(`(?:https?:\/\/)?steamcommunity\.com\/(?:profiles|id)\/[a-zA-Z0-9]+`);
-    // Run checks
-    return new Promise(function(resolve, reject) {
-        // Ensure the given Steam Profile URL is valid
-        let result = steamURLReg.test(steamURL);
-        dlog(`${steamURL} valid? ${result}`);
-        resolve(result);
-    });
+	// Steam Profile URL Regex
+	const steamURLReg = new RegExp('(?:https?://)?steamcommunity.com/(?:profiles|id)/[a-zA-Z0-9]+');
+	// Run checks
+	return new Promise(function(resolve) {
+		// Ensure the given Steam Profile URL is valid
+		const result = steamURLReg.test(steamURL);
+		dlog(`${steamURL} valid? ${result}`);
+		resolve(result);
+	});
 };
