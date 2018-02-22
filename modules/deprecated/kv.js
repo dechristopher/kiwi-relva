c27a2ab025a10c9abe21c6a8d3924b31ab619ee3
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:35:34 -0500

    ESLint cleanup -  kv.js

--- modules/deprecated/kv.js
+++ modules/deprecated/kv.js
@@ -1,35 +1,35 @@
-let Collection = require('discord.js').Collection;
+const Collection = require('discord.js').Collection;
 
 class KV {
-    constructor() {
-        /**
+	constructor() {
+		/**
          * All of the {@link Party} objects that have been cached at any point, mapped by their IDs
          * @type {Collection<string, Party>}
          */
-        this.parties = new Collection();
+		this.parties = new Collection();
 
-        /**
+		/**
          * @type {Collection<string, Channel>}
          */
-        this.matchRooms = new Collection();
+		this.matchRooms = new Collection();
 
-        /**
+		/**
          * @type {Collection<string, QueueEntity>}
          */
-        this.queue = new Collection();
-    }
+		this.queue = new Collection();
+	}
 
-    parties() {
-        return this.parties;
-    }
+	parties() {
+		return this.parties;
+	}
 
-    matchRooms() {
-        return this.matchRooms;
-    }
+	matchRooms() {
+		return this.matchRooms;
+	}
 
-    queue() {
-        return this.queue;
-    }
+	queue() {
+		return this.queue;
+	}
 }
 
 module.exports = KV;
