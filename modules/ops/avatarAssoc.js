e1538710a74114602469393eba74475487c7bc1f
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:38:01 -0500

    ESLint cleanup - avatarAssoc.js

--- modules/ops/avatarAssoc.js
+++ modules/ops/avatarAssoc.js
@@ -10,39 +10,52 @@ const dlog = require('../util/log.js');
  * @param {string} avatarURL link to the user's Discord avatar
  * @param {Object} dbconn reference to the database connection
  * @param {Object} cconn reference to the cache connection
- * @param {boolean} debug debug mode
  */
-module.exports = function(userID, avatarURL, dbconn, cconn, debug) {
-    // Check to see if the cache contains an entry for the user's avatar
-    cconn.hexists('avatars', userID, function(err, reply) {
-        dlog(`Avatar Exists (${userID}): ${reply}`);
-        if (reply === 1) {
-            // Get the avatar URL from the cache to check if it's the same
-            cconn.hget('avatars', userID, function(err, reply) {
-                if (reply === avatarURL) {
-                    // Do nothing :D
-                    dlog(`Avatar for ${userID} already set properly. Not hitting db.`);
-                    return;
-                } else {
-                    // Update the changed avatar in the database
-                    dbconn.query('UPDATE `users` SET `discord_avatar_url` = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
-                        dlog(`Set ${userID} avatar to ${avatarURL} in db`);
-                    });
-                    // Update the changed avatar in the cache
-                    cconn.hset('avatars', userID, avatarURL, function(err, reply) {
-                        dlog(`Set ${userID} avatar to ${avatarURL} in cache`);
-                    });
-                }
-            });
-        } else {
-            // Set the avatar in the database
-            dbconn.query('UPDATE `users` SET `discord_avatar_url` = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error, results, fields) {
-                dlog(`Set ${userID} avatar to ${avatarURL} in db`);
-            });
-            // Set the avatar in the cache
-            cconn.hset('avatars', userID, avatarURL, function(err, reply) {
-                dlog(`Set ${userID} avatar to ${avatarURL} in cache`);
-            });
-        }
-    });
+module.exports = function(userID, avatarURL, dbconn, cconn) {
+	// Check to see if the cache contains an entry for the user's avatar
+	cconn.hexists('avatars', userID, function(err, reply) {
+		dlog(`Avatar Exists (${userID}): ${reply}`);
+		if (reply === 1) {
+			// Get the avatar URL from the cache to check if it's the same
+			cconn.hget('avatars', userID, function(err, getReply) {
+				if (getReply === avatarURL) {
+					// Do nothing :D
+					dlog(`Avatar for ${userID} already set properly. Not hitting db.`);
+					return;
+				}
+				else {
+					// Update the changed avatar in the database
+					dbconn.query('UPDATE `users` SET `discord_avatar_url` = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error) {
+						dlog(`Set ${userID} avatar to ${avatarURL} in db`);
+						if (error) {
+							dlog(`Set avatar in db ERROR: ${error}`);
+						}
+					});
+					// Update the changed avatar in the cache
+					cconn.hset('avatars', userID, avatarURL, function(error) {
+						dlog(`Set ${userID} avatar to ${avatarURL} in cache`);
+						if (error) {
+							dlog(`Set avatar in cache ERROR: ${error}`);
+						}
+					});
+				}
+			});
+		}
+		else {
+			// Set the avatar in the database
+			dbconn.query('UPDATE `users` SET `discord_avatar_url` = ? WHERE `discord_id` = ?', [avatarURL, userID], function(error) {
+				dlog(`Set ${userID} avatar to ${avatarURL} in db`);
+				if(error) {
+					dlog(`Set avatar in db ERROR: ${error}`);
+				}
+			});
+			// Set the avatar in the cache
+			cconn.hset('avatars', userID, avatarURL, function(error) {
+				dlog(`Set ${userID} avatar to ${avatarURL} in cache`);
+				if (error) {
+					dlog(`Set avatar in cache ERROR: ${error}`);
+				}
+			});
+		}
+	});
 };
