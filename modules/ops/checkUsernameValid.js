501f8311b368149c95b4d6c4c6d66b12bed631c4
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-21 21:51:43 -0500

    Documentation revamp - checkUsernameValid.js

--- modules/ops/checkUsernameValid.js
+++ modules/ops/checkUsernameValid.js
@@ -1,19 +1,18 @@
-// kiwi/relva/modules/ops/checkLinkedNoUsername.js - Created February 15th, 2018
+// kiwi/relva/modules/ops/checkUsernameValid.js - Created February 15th, 2018
 
 // Custom Modules
 const dlog = require('../util/dlog.js');
 
-/*
-    Account link checker (no username set)
-    - userID (string): the user's Discord ID
-    - dbconn (Object): reference to the database connection
-    - debug (boolean): whether or not to show debug output
-*/
+/**
+ * Username validity checker
+ * @param {string} username the username to check
+ * @returns {Promise<boolean>} whether or not the username is valid
+ */
 module.exports = function(username) {
     return new Promise(function(resolve, reject) {
-        // Ensure user has provided a valid username
-        // alpha-numerics, underscores, and dashes only
-        // more than two characters
+        // Ensure user has provided a valid username:
+        //  - alpha-numerics, underscores, and dashes only
+        //  - more than two characters
         let usernameRegex = /^[a-zA-Z0-9-_]+$/;
         let result = (usernameRegex.test(username)) && (username.length >= 2);
         dlog(`${username} valid? ${result}`);
