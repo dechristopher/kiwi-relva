6f78cdb21d52dc6681630982a7dd282dc648b344
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:39:53 -0500

    ESLint cleanup - grabSteamID.js

--- modules/ops/grabSteamID.js
+++ modules/ops/grabSteamID.js
@@ -21,28 +21,28 @@ const dlog = require('../util/dlog.js');
  *  }
  */
 module.exports = function(steamURL) {
-    return new Promise(function(resolve, reject) {
-        let nugget = {};
-        // Snag some info from the XML API
-        got(`${steamURL}?xml=1`).then(resp => {
-            dlog('Hit Steam XML -> ' + steamURL);
-            // Parse the XML as JSON
-            parser(resp.body, (err, results) => {
-                if (err) {
-                    log(`[SID PARSE ERROR] ${steamURL} -> ${err}`);
-                    nugget.error = `Failed to parse Steam Profile URL. Please try again later on after contacting \`<@119966322523242497>\` immediately. [CODE: K76]`;
-                    nugget.ids = {};
-                    resolve(nugget);
-                }
-                nugget.error = undefined;
-                // Convert the steamID64 into the other two formats
-                nugget.ids = {
-                    steamid: steam.convertToText(results.profile.steamID64[0]),
-                    steamid64: results.profile.steamID64[0],
-                    steamid3: steam.convertToNewFormat(steam.convertToText(results.profile.steamID64[0]))
-                };
-                resolve(nugget);
-            });
-        });
-    });
+	return new Promise(function(resolve) {
+		const nugget = {};
+		// Snag some info from the XML API
+		got(`${steamURL}?xml=1`).then(resp => {
+			dlog('Hit Steam XML -> ' + steamURL);
+			// Parse the XML as JSON
+			parser(resp.body, (err, results) => {
+				if (err) {
+					log(`[SID PARSE ERROR] ${steamURL} -> ${err}`);
+					nugget.error = 'Failed to parse Steam Profile URL. Please try again later on after contacting `<@119966322523242497>` immediately. [CODE: K76]';
+					nugget.ids = {};
+					resolve(nugget);
+				}
+				nugget.error = undefined;
+				// Convert the steamID64 into the other two formats
+				nugget.ids = {
+					steamid: steam.convertToText(results.profile.steamID64[0]),
+					steamid64: results.profile.steamID64[0],
+					steamid3: steam.convertToNewFormat(steam.convertToText(results.profile.steamID64[0])),
+				};
+				resolve(nugget);
+			});
+		});
+	});
 };
