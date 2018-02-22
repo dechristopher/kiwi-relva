9a98525eec4d7f48b658b1adc5999416b153323d
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:40:58 -0500

    ESLint cleanup - Solo.js

--- modules/types/Solo.js
+++ modules/types/Solo.js
@@ -1,19 +1,19 @@
 // kiwi/relva/modules/types/Solo.js - Created February 17th, 2018
 
 // Custom Modules
-let QueueEntity = require('./QueueEntity.js');
+const QueueEntity = require('./QueueEntity.js');
 
 class Solo extends QueueEntity {
 
-    /**
+	/**
      * Creates a solo queue player QueueEntity
      * @param {GuildMember} player the solo player that is queued
      * @param {Object} params player configuration options
      */
-    constructor(player, params = { region: "us-e" }) {
-        super({ type: "solo", region: params.region });
-        super.addMember(player);
-    }
+	constructor(player, params = { region: 'us-e' }) {
+		super({ type: 'solo', region: params.region });
+		super.addMember(player);
+	}
 }
 
 module.exports = Solo;
