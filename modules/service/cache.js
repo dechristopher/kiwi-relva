707af88da82b87246ddae7c1400809ee0c8ef33c
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:40:12 -0500

    ESLint cleanup - cache.js

--- modules/service/cache.js
+++ modules/service/cache.js
@@ -1,13 +1,14 @@
 // kiwi/relva/modules/db.js - Created February 6th, 2018
 
 // NPM Modules
-const redis = require('redis'); //https://github.com/NodeRedis/node_redis
+const redis = require('redis');
+// https://github.com/NodeRedis/node_redis
 
 // Custom Modules
 const log = require('../util/log.js');
 
 class RedisConnection {
-    /**
+	/**
      * Builds a Redis cache connection
      * @param {Object} options connection options
      * @param {string} options.host db host to connect to
@@ -15,27 +16,27 @@ class RedisConnection {
      * @param {string} options.password db auth password
      * @param {string} options.db database to use
      */
-    constructor(options) {
-        this.connection = redis.createClient(options);
+	constructor(options) {
+		this.connection = redis.createClient(options);
 
-        this.connection.on("error", function(err) {
-            log(`[cache] Error: ${err}`);
-            process.exit(1);
-        });
+		this.connection.on('error', function(err) {
+			log(`[cache] Error: ${err}`);
+			process.exit(1);
+		});
 
-        this.connection.on("connect", function() {
-            log(`[cache] Connection established`);
-        });
+		this.connection.on('connect', function() {
+			log('[cache] Connection established');
+		});
 
-        this.connection.on("ready", function() {
-            //this.connection.auth(options.password);
-            log(`[cache] Cache ready`);
-        });
-    }
+		this.connection.on('ready', function() {
+			// this.connection.auth(options.password);
+			log('[cache] Cache ready');
+		});
+	}
 
-    conn() {
-        return this.connection;
-    }
+	conn() {
+		return this.connection;
+	}
 }
 
 module.exports = RedisConnection;
