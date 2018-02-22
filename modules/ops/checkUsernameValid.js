9fc2769042beba5f4b36049cdacbb0c51ddb24b9
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:38:34 -0500

    ESLint cleanup - checkUsernameValid.js

--- modules/ops/checkUsernameValid.js
+++ modules/ops/checkUsernameValid.js
@@ -9,13 +9,13 @@ const dlog = require('../util/dlog.js');
  * @returns {Promise<boolean>} whether or not the username is valid
  */
 module.exports = function(username) {
-    return new Promise(function(resolve, reject) {
-        // Ensure user has provided a valid username:
-        //  - alpha-numerics, underscores, and dashes only
-        //  - more than two characters
-        let usernameRegex = /^[a-zA-Z0-9-_]+$/;
-        let result = (usernameRegex.test(username)) && (username.length >= 2);
-        dlog(`${username} valid? ${result}`);
-        resolve(result);
-    });
+	return new Promise(function(resolve) {
+		// Ensure user has provided a valid username:
+		//  - alpha-numerics, underscores, and dashes only
+		//  - more than two characters
+		const usernameRegex = /^[a-zA-Z0-9-_]+$/;
+		const result = (usernameRegex.test(username)) && (username.length >= 2);
+		dlog(`${username} valid? ${result}`);
+		resolve(result);
+	});
 };
