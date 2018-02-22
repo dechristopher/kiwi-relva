92bf1e17ba98895991e0f15b7942f0dfa19e2f6f
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:40:38 -0500

    ESLint cleanup - Party.js

--- modules/types/Party.js
+++ modules/types/Party.js
@@ -1,10 +1,10 @@
 // kiwi/relva/modules/types/Party.js - Created February 17th, 2018
 
 // Custom Modules
-let QueueEntity = require('./QueueEntity.js');
+const QueueEntity = require('./QueueEntity.js');
 
 // Custom Modules
-let genID = require('../util/genID.js');
+const genID = require('../util/genID.js');
 
 /**
  * A QueueEntity that handles parties of multiple players
@@ -12,40 +12,40 @@ let genID = require('../util/genID.js');
  */
 class Party extends QueueEntity {
 
-    /**
+	/**
      * Creates a Party QueueEntity
      * @param {GuildMember} leader
      * @param {Object} params party configuration options
      */
-    constructor(leader, params = { region: "us-e" }) {
-        super({ type: "party", region: params.region });
-        super.addMember(leader);
-        this.leader = leader;
-        this.joinCode = genID(4, { prefix: false });
-    }
+	constructor(leader, params = { region: 'us-e' }) {
+		super({ type: 'party', region: params.region });
+		super.addMember(leader);
+		this.leader = leader;
+		this.joinCode = genID(4, { prefix: false });
+	}
 
-    /**
+	/**
      * @returns {string} the leader's id
      */
-    leaderID() {
-        return this.leader.id;
-    }
+	leaderID() {
+		return this.leader.id;
+	}
 
-    /**
+	/**
      * Sets a member of the party as the group leader
      * @param {GuildMember} member
      */
-    newLeader(member) {
-        this.leader = member;
-    }
+	newLeader(member) {
+		this.leader = member;
+	}
 
-    /**
+	/**
      * @returns {string} the newly generated join code of the party
      */
-    newJoinCode() {
-        this.joinCode = genID(4, { prefix: false });
-        return this.joinCode;
-    }
+	newJoinCode() {
+		this.joinCode = genID(4, { prefix: false });
+		return this.joinCode;
+	}
 
 }
 
