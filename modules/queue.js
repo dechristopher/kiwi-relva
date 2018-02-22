b311ed2400083f45b5aab36410357b0baee9586d
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-22 18:34:31 -0500

    ESLint cleanup - queue.js

--- modules/queue.js
+++ modules/queue.js
@@ -1,28 +1,28 @@
 // kiwi/relva/modules/queue.js - Created February 16th, 2018
 
 // NPM Modules
-let Collection = require('discord.js').Collection;
+const Collection = require('discord.js').Collection;
 
 // Core Modules
 const util = require('util');
 const EventEmitter = require('events');
 
 class Queue {
-    constructor() {
-        EventEmitter.call(this);
-        /**
+	constructor() {
+		EventEmitter.call(this);
+		/**
          * @type {Collection<string, QueueEntity>}
          */
-        this.queue = new Collection();
-    }
+		this.queue = new Collection();
+	}
 
-    /**
+	/**
      * Emits a single event with no payload
      * @param {string} type type of event
      */
-    emitOne(type) {
-        this.emit(type);
-    }
+	emitOne(type) {
+		this.emit(type);
+	}
 }
 
 // Inherit prototypes from EventEmitter
