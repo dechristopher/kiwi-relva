26002352ea35bed6403b1e64365270d9463340fd
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 01:30:14 -0500

    Implemented link command handler module

---
+++ commands/linking/link.js
@@ -0,0 +1,14 @@
+// kiwi/relva/commands/linking/link.js - Created February 22nd, 2018
+
+const { opUser } = require('../../index.js');
+
+module.exports = {
+	name: 'link',
+	aliases: ['register', 'connect', 'signup'],
+	description: 'Link Steam Profile to Discord user.',
+	execute(message, args) {
+		opUser.linkAccount(message.author.username, message.author.id, args[0]).then(resp => {
+			message.reply(resp);
+		});
+	},
+};
