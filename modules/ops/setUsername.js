7fa2f0181a5ff21e7cd059048541d0dc51d8a490
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 01:26:47 -0500

    ~ Changed setUsername.js responses to work with direct user responses

--- modules/ops/setUsername.js
+++ modules/ops/setUsername.js
@@ -24,32 +24,32 @@ module.exports = function(userID, username, dbconn) {
 		//          - Else set username in DB and then set user role to 'PUG User'
 		checkLinkedNoUsername(userID, dbconn).then(linked => {
 			if (!linked) {
-				resolve('You have to link your SteamID with `!link <Steam Profile URL>` before setting your username.');
+				resolve('you have to link your SteamID with `!link <Steam Profile URL>` before setting your username.');
 				return;
 			}
 			else {
 				checkUsernameValid(username).then(valid => {
 					if (!valid) {
-						resolve('Your desired username must only contain `letters`, `numbers`, `dashes`, and `underscores` and must be `two or more characters long`.');
+						resolve('your desired username must only contain `letters`, `numbers`, `dashes`, and `underscores` and must be `two or more characters long`.');
 					}
 					else {
 						dbconn.query('SELECT steam_id FROM `users` WHERE `username` = ?', [username], function(checkError, checkResults) {
 							if (checkError) {
 								log('setUsername Error [0]: ' + checkError);
-								resolve('Sorry, something weird happened on our end. Contact `<@119966322523242497>` immediately and try again shortly. `[CODE: K91]`');
+								resolve('sorry but something weird happened on our end. Contact `<@119966322523242497>` immediately and try again shortly. `[CODE: K91]`');
 								return;
 							}
 							if (checkResults.length === 1) {
-								resolve(`The username \`${username}\` is already taken. Try another.`);
+								resolve(`the username \`${username}\` is already taken. Try another.`);
 							}
 							else {
 								dbconn.query('UPDATE `users` SET `username` = ? WHERE `discord_id` = ?', [username, userID], function(updateError) {
 									if (updateError) {
 										log('setUsername Error [1]: ' + updateError);
-										resolve('Sorry, something weird happened on our end. Contact `<@119966322523242497>` immediately and try again shortly. `[CODE: K92]`');
+										resolve('sorry but something weird happened on our end. Contact `<@119966322523242497>` immediately and try again shortly. `[CODE: K92]`');
 										return;
 									}
-									resolve(`Username set successfully to \`${username}\`, you'll be removed from this channel shortly and added into the #kiwipugs channel. Use \`!help\` to learn how to use the PUG system. Have fun!`);
+									resolve(`your username was set successfully to \`${username}\`, you'll be removed from this channel shortly and added into the #kiwipugs channel. Use \`!help\` to learn how to use the PUG system. Have fun!`);
 								});
 							}
 						});
