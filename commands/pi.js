dd3d5cc60e818690677dc96282b63a3d46e94455
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 01:29:52 -0500

    Implemented pi command handler module

---
+++ commands/pi.js
@@ -0,0 +1,12 @@
+// kiwi/relva/commands/pi.js - Created February 22nd, 2018
+
+module.exports = {
+	name: 'pi',
+	description: 'For those that need to know the ratio of a circle\'s circumference to its diameter.',
+	messages: {
+		replyPi: '3.14159265359...',
+	},
+	execute(message) {
+		message.channel.send(`${this.messages.replyPi}`);
+	},
+};
