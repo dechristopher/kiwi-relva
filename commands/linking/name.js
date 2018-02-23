1bce5c7251379efdd0cf20d44ec617c07d84ac60
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 01:30:29 -0500

    Implemented name command handler module

---
+++ commands/linking/name.js
@@ -0,0 +1,17 @@
+// kiwi/relva/commands/linking/name.js - Created February 22nd, 2018
+
+const { opUser, setPUGRole } = require('../../index.js');
+
+module.exports = {
+	name: 'name',
+	aliases: ['username'],
+	description: 'Sets the KIWI username and PUG Player role of a Discord user.',
+	execute(message, args) {
+		opUser.setUsername(message.author.id, args[0]).then(resp => {
+			if (resp.includes('#kiwipugs')) {
+				setPUGRole(message.author);
+			}
+			message.reply(resp);
+		});
+	},
+};
