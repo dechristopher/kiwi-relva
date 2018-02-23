53fe0fe338230bcae8e1db6a139d36a1fe00cf00
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 09:25:02 -0500

    Made pi more precise so it's not recognized as a phone number on mobile

--- commands/pi.js
+++ commands/pi.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'pi',
 	description: 'For those that need to know the ratio of a circle\'s circumference to its diameter.',
 	messages: {
-		replyPi: '3.14159265359...',
+		replyPi: '3.141592653589793238462...',
 	},
 	execute(message) {
 		message.channel.send(`${this.messages.replyPi}`);
