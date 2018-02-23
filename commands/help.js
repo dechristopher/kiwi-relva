767131b6d1289c6823ccfebd917b3f339fef93cc
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-23 01:28:44 -0500

    Implemented help command handler module

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+// kiwi/relva/commands/help.js - Created February 23nd, 2018
+
+const help = require('../modules/help.js');
+
+module.exports = {
+	name: 'help',
+	aliases: ['h'],
+	description: 'Gives assistance with commands.',
+	messages: {
+		replyHelp: 'available commands are: `!ping`, `!(q)ueue`, `!(p)arty`, `!(a)bout`, `!(h)elp` - for command details type `!help <command>`',
+	},
+	execute(message, args) {
+		message.reply((args.length > 0) ? help(args) : this.messages.replyHelp);
+	},
+};
