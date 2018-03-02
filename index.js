683b6942a25a5af5b25537efb90e5bb9ba325f5a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-01 19:49:23 -0500

    Removed old command runner code

--- index.js
+++ index.js
@@ -184,6 +184,8 @@ bot.on('message', message => {
 	const args = message.content.slice(conf.prefix.length).split(/ +/);
 	// Begin fake bot typing
 	message.channel.startTyping();
+	// const cmd = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
 	// Get user privilege level
 	const privLevel = priv(message.author.id);
@@ -256,71 +258,6 @@ bot.on('message', message => {
 		}
 		catch (error) {
 			log(error);
-			message.reply(`our fault, we hit an error somewhere... ${conf.server.adminMention} will look into it further!`);
-		} */
-		// TESTING
-
-		if (isLinked) {
-			dlog(`User linked: ${message.author.id} - yes`);
-			// Perform avatar association transaction
-			opUser.avatarAssoc(message.author.id, message.author.avatarURL);
-
-			// Run commands
-			switch (cmd) {
-			case 'ping':
-				bot.commands.get('ping').execute(message, args);
-				break;
-
-			case 'h':
-			case 'help':
-				bot.commands.get('help').execute(message, args);
-				break;
-
-			case 'a':
-			case 'about':
-				bot.commands.get('about').execute(message);
-				break;
-
-			case 'link':
-				bot.commands.get('link').execute(message, args);
-				break;
-
-			case 'q':
-			case 'queue':
-				reply(enqueue(message.author.id));
-				break;
-
-			case 'p':
-			case 'party':
-				// Party operations
-				reply(party(args));
-				break;
-
-			case 'pi':
-				bot.commands.get('pi').execute(message);
-				break;
-
-			case 'admin':
-				bot.commands.get('admin').execute(message);
-				break;
-
-			default:
-				reply(`\`!${cmd}\` isn't a valid command. Use !help to learn more.`);
-				break;
-			}
-		}
-		else {
-			dlog(`User linked: ${message.author.id} - no`);
-			// Check for link attempt
-			if (cmd === 'link') {
-				bot.commandsLink.get('link').execute(message, args);
-			}
-			else if (cmd === 'name') {
-				bot.commandsLink.get('name').execute(message, args);
-			}
-			else {
-				reply(strMsgNotLinked);
-			}
 			message.reply(`our fault, we hit an error somewhere... ${conf.server.adminMention} will look into it further! [CODE: K17]`);
 		}
 		// [/TESTING]
