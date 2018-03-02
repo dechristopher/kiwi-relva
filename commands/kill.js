62ac6427fd24bc566f9f54676f6f8bb5cf08b2be
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-01 20:51:40 -0500

    Implemented kill command

---
+++ commands/kill.js
@@ -0,0 +1,22 @@
+// kiwi/relva/commands/kill.js - Created March 1st, 2018
+
+const { terminate } = require('../index');
+
+module.exports = {
+	name: 'kill',
+	aliases: ['k'],
+	description: 'Terminates the bot immediately.',
+	messages: {
+		replyKill: 'I\'m shutting down now. Bye...',
+		replyFakeNotCommand: '`kill` isn\'t a valid command. Use !help to learn more.',
+	},
+	execute(message, args, privLevel) {
+		if(privLevel === 2) {
+			message.reply(this.messages.replyKill);
+			terminate();
+		}
+		else {
+			message.reply(this.messages.replyFakeNotCommand);
+		}
+	},
+};
