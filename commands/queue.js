134918a7dc635337a8e9111cdf8766aa30b6fbf2
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-13 19:53:23 -0400

    Began working on queue subcommands

--- commands/queue.js
+++ commands/queue.js
@@ -5,9 +5,38 @@ module.exports = {
 	aliases: ['q'],
 	description: 'Allows a user or party to join the queue.',
 	messages: {
+		replyInvalidArgument: 'that\'s an invalid operation! Valid queue operations are `(j)oin`, `(l)eave`, and `(i)nfo`',
 		replyQueueNotAvailable: 'the queue isn\'t available yet. Check back later after I\'m done being made!',
 	},
-	execute(message) {
-		message.reply(`${this.messages.replyQueueNotAvailable}`);
+	subcommands: ['j', 'join', 'l', 'leave', 'i', 'info'],
+	execute(message, args) {
+		const sub = this.subcommands.find(args[0]);
+
+		// Error out if subcommand not valid
+		if(!sub) {
+			message.reply(`${this.messages.replyInvalidArgument}`);
+		}
+
+		// Decipher and run subcommands
+		if(sub.indexOf('j') === 0) {
+			return this.join();
+		}
+		else if(sub.indexOf('l') === 0) {
+			return this.leave();
+		}
+		else if(sub.indexOf('i') === 0) {
+			return this.info();
+		}
+
+		// message.reply(`${this.messages.replyQueueNotAvailable}`);
+	},
+	join() {
+		return;
+	},
+	leave() {
+		return;
+	},
+	info() {
+		return;
 	},
 };
