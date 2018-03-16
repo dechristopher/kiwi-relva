cc0db151a1101f47a803a1148b0b8ca13b4a16c2
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-16 08:58:58 -0400

    Fake typing stopper - about.js

--- commands/about.js
+++ commands/about.js
@@ -5,9 +5,10 @@ module.exports = {
 	aliases: ['a'],
 	description: 'Displays some basic information about KIWI Bot.',
 	messages: {
-		replyAbout: 'how the heck are ya? I\'m KIWI Bot! Use me like your sick puppet and bend me to your will to use the KIWI PUG service. Use `!help` to learn what I can do.',
+		replyAbout: 'how the heck are ya? I\'m KIWI Bot! Bend me to your will to use the KIWI PUG system. Use `!help` to learn what I can do.',
 	},
 	execute(message) {
 		message.reply(`${this.messages.replyAbout}`);
+		message.channel.stopTyping();
 	},
 };
