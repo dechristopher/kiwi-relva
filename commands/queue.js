b692ba1d7809a3bda5d2273b91a5c6e99c7d9a4f
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-16 09:06:12 -0400

    Implemented queue module base structure

--- commands/queue.js
+++ commands/queue.js
@@ -3,18 +3,20 @@
 module.exports = {
 	name: 'queue',
 	aliases: ['q'],
-	description: 'Allows a user or party to join the queue.',
+	description: 'Allows a player or party to join the queue.',
 	messages: {
 		replyInvalidArgument: 'that\'s an invalid operation! Valid queue operations are `(j)oin`, `(l)eave`, and `(i)nfo`',
 		replyQueueNotAvailable: 'the queue isn\'t available yet. Check back later after I\'m done being made!',
 	},
 	subcommands: ['j', 'join', 'l', 'leave', 'i', 'info'],
-	execute(message, args) {
+	execute(message, args, privLevel) {
 		const sub = this.subcommands.find(args[0]);
 
 		// Error out if subcommand not valid
 		if(!sub) {
 			message.reply(`${this.messages.replyInvalidArgument}`);
+			message.channel.stopTyping();
+			return;
 		}
 
 		// Decipher and run subcommands
@@ -25,7 +27,7 @@ module.exports = {
 			return this.leave();
 		}
 		else if(sub.indexOf('i') === 0) {
-			return this.info();
+			return this.info(privLevel);
 		}
 
 		// message.reply(`${this.messages.replyQueueNotAvailable}`);
@@ -37,6 +39,12 @@ module.exports = {
 		return;
 	},
 	info() {
+		/*
+			Regular user: @you, there are currently X players looking for a match in your region.
+					 VIP: @you, there are currently X players looking for a match in your region.
+				   Admin: @you, there are currently X players in X prties looking for a match.
+					  - : DM -> rich text showing all players and parties
+		*/
 		return;
 	},
 };
